860ede267591927ebf2aa4b215ffbcd023c83f46
Dheeram Kallem <dheeram.kallem@gmail.com>
2020-06-09 09:17:06 -0400

    added more log statements

--- server/index.js
+++ server/index.js
@@ -49,6 +49,7 @@ app.get('/values/all', async (req, res) => {
 
 app.get('/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
+    console.log('********* values from redisClient ', values);
     res.send(values);
   });
 });
